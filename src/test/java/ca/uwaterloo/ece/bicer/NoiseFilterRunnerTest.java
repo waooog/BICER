1c905fa2f9e0c5aab7a328e6ec1df318e6f1f022
lifove <jc@lifove.net>
2016-06-02 11:47:27 -0400

    Update the test case and the example change data

--- src/test/java/ca/uwaterloo/ece/bicer/NoiseFilterRunnerTest.java
+++ src/test/java/ca/uwaterloo/ece/bicer/NoiseFilterRunnerTest.java
@@ -13,7 +13,7 @@ public class NoiseFilterRunnerTest {
         
     	NoiseFilterRunner runner = new NoiseFilterRunner();
     	
-    	String [] args ={"-d","data/exampleBIChanges.txt", "-g", "https://github.com/lifove/BICER.git"};
+    	String [] args ={"-d","data/exampleBIChanges.txt", "-g", System.getProperty("user.home") + "/git/BICER"};
     	
     	runner.run(args);
     	
