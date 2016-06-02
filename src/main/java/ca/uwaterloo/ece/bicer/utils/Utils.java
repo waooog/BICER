1c9923d6d4147a7a241be13a92c88692194631fb
lifove <jc@lifove.net>
2016-06-02 11:31:22 -0400

    Add fetchBlob to get String contents of a file with a specific sha1 and path

--- src/main/java/ca/uwaterloo/ece/bicer/utils/Utils.java
+++ src/main/java/ca/uwaterloo/ece/bicer/utils/Utils.java
@@ -5,6 +5,16 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectReader;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.TreeWalk;
+
 public class Utils {
 	static public ArrayList<String> getLines(String file,boolean removeHeader){
 		ArrayList<String> lines = new ArrayList<String>();
@@ -26,5 +36,38 @@ public class Utils {
 			lines.remove(0);
 		
 		return lines;
+	}
+	
+	static public String fetchBlob(Repository repo, String revSpec, String path) throws MissingObjectException, IncorrectObjectTypeException,
+    IOException {
+
+		// Resolve the revision specification
+		final ObjectId id = repo.resolve(revSpec);
+		
+		// Makes it simpler to release the allocated resources in one go
+		ObjectReader reader = repo.newObjectReader();
+		
+		try {
+		    // Get the commit object for that revision
+		    RevWalk walk = new RevWalk(reader);
+		    RevCommit commit = walk.parseCommit(id);
+		    walk.close();
+		
+		    // Get the revision's file tree
+		    RevTree tree = commit.getTree();
+		    // .. and narrow it down to the single file's path
+		    TreeWalk treewalk = TreeWalk.forPath(reader, path, tree);
+		
+		    if (treewalk != null) {
+		        // use the blob id to read the file's data
+		        byte[] data = reader.open(treewalk.getObjectId(0)).getBytes();
+		        return new String(data, "utf-8");
+		    } else {
+		        return "";
+		    }
+
+		} finally {
+		    reader.close();
+		}
 	}
 }
