aad37da13447968ca0dede798d02028d50d1a3aa
lifove <jc@lifove.net>
2016-06-09 17:10:54 -0400

    Update UtilTest to test DiffEntry

--- src/test/java/ca/uwaterloo/ece/bicer/UtilsTest.java
+++ src/test/java/ca/uwaterloo/ece/bicer/UtilsTest.java
@@ -2,6 +2,10 @@ package ca.uwaterloo.ece.bicer;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.jgit.api.Git;
 import org.junit.Test;
 
 import ca.uwaterloo.ece.bicer.utils.Utils;
@@ -16,6 +20,15 @@ public class UtilsTest {
 		line = Utils.removeLineComments(line);
 		System.out.println(line);
 		assertEquals("public  static String  removeLineComments(String line) { ",line);	
+		
+		Git git;
+		try {
+			git = Git.open( new File( System.getProperty("user.home") + "/git/BICER") );
+			Utils.diff(git, "6a25685e6a4dcf379718057b2465d998242f6ff6~1", "6a25685e6a4dcf379718057b2465d998242f6ff6", "src/main/java/ca/uwaterloo/ece/bicer/noisefilters/NameChange.java");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
