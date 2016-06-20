5698b827f771703db8d071ce93ed08def65fec08
JC <jc@lifove.net>
2016-06-20 19:28:19 -0400

    Update PositionChange filter to support method move

--- src/main/java/ca/uwaterloo/ece/bicer/noisefilters/PositionChange.java
+++ src/main/java/ca/uwaterloo/ece/bicer/noisefilters/PositionChange.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.FieldDeclaration;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 
 import ca.uwaterloo.ece.bicer.data.BIChange;
@@ -71,7 +72,8 @@ public class PositionChange implements Filter {
 			return true;
 
 		// TODO java methods
-
+		if(isPositionChangeOfMethod())
+			return true;
 		// TODO c constants / variables
 
 		// TODO c functions / structs
@@ -80,12 +82,12 @@ public class PositionChange implements Filter {
 	}
 
 	private boolean isPositionChangeOfFields() {
-		
+
 		int biLineNum = biChange.getLineNumInPrevFixRev();
 
 		ArrayList<FieldDeclaration> lstBIFieldDeclaration = preFixWholeCodeAST.getFieldDeclarations();
 		ArrayList<FieldDeclaration> lstFixFieldDeclaration = fixedWholeCodeAST.getFieldDeclarations();
-		
+
 		FieldDeclaration biFieldDeclaration = null;
 		for(ASTNode varDecFragNode:lstBIFieldDeclaration){
 			if(biLineNum==preFixWholeCodeAST.getCompilationUnit().getLineNumber(varDecFragNode.getStartPosition())){
@@ -98,8 +100,58 @@ public class PositionChange implements Filter {
 			if(biFieldDeclaration!=null
 					&& biFieldDeclaration.toString().equals(varDecFragNode.toString())
 					&& ((TypeDeclaration)biFieldDeclaration.getParent()).getName().toString().equals
+					(
+							((TypeDeclaration)varDecFragNode.getParent()).getName().toString()
+							)
+					)
+				return true;
+		}
+
+		return false;
+	}
+
+	private boolean isPositionChangeOfMethod() {
+
+		// No need to consider a deleted line in a BI change
+		if(!biChange.getIsAddedLine())
+			return false;
+
+		ArrayList<MethodDeclaration> lstMethodDeclaration = preFixWholeCodeAST.getMethodDeclarations();
+		ArrayList<MethodDeclaration> lstFixMethodDeclaration = fixedWholeCodeAST.getMethodDeclarations();
+
+		// (1) get method that contains a BI line that is not a line for method declaration.
+		MethodDeclaration preFixMethodDecNodeHavingBILine=null;
+		int potentialBeginALineNum = -1; // deleted line range start line num
+		int potentialEndALineNum = -1; // deleted line range end line num
+
+		for(MethodDeclaration methodDecNode:lstMethodDeclaration){
+			int beginLineNum = preFixWholeCodeAST.getCompilationUnit().getLineNumber(methodDecNode.getStartPosition());
+			int endLineNum = preFixWholeCodeAST.getCompilationUnit().getLineNumber(methodDecNode.getStartPosition()+methodDecNode.getLength());
+			if(biChange.getLineNumInPrevFixRev()>=beginLineNum && biChange.getLineNumInPrevFixRev()<= endLineNum){
+				preFixMethodDecNodeHavingBILine = methodDecNode;
+				potentialBeginALineNum = beginLineNum;
+				potentialEndALineNum = endLineNum;
+				break;
+			}
+		}
+
+		if(preFixMethodDecNodeHavingBILine==null) // biLine is not a line in a method.
+			return false;
+
+		if(biChange.getEdit()==null)
+			return false;
+
+		// (2) check all lines for the method are removed.
+		if(!((potentialBeginALineNum > biChange.getEdit().getBeginA()) && (potentialEndALineNum <= biChange.getEdit().getEndA()))){
+			return false;
+		}
+
+		// (3) check if the same method exists
+		for(MethodDeclaration methodDec:lstFixMethodDeclaration){
+			if(preFixMethodDecNodeHavingBILine.toString().equals(methodDec.toString())
+					&& ((TypeDeclaration)preFixMethodDecNodeHavingBILine.getParent()).getName().toString().equals
 							(
-								((TypeDeclaration)varDecFragNode.getParent()).getName().toString()
+									((TypeDeclaration)methodDec.getParent()).getName().toString()
 							)
 			)
 				return true;
